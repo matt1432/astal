9c8f343a6ed9ecaa40e3140d22f67afbe509123a
matt1432 <matt@nelim.org>
2024-12-26 01:15:46 -0500

    fix(actionGroup): remove unused setter

--- lang/gjs/src/gtk3/astalify.ts
+++ lang/gjs/src/gtk3/astalify.ts
@@ -37,7 +37,6 @@ export default function astalify<
         set noImplicitDestroy(value: boolean) { this[noImplicitDestroy] = value }
 
         set insertActionGroup([prefix, group]: ActionGroup) { this.insert_action_group(prefix, group) }
-        set_insert_action_group(actionGroup: ActionGroup) { this.actionGroup = actionGroup }
 
         protected getChildren(): Array<Gtk.Widget> {
             if (this instanceof Gtk.Bin) {
