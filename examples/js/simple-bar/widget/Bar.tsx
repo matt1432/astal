cbed01fc6c08412100c4e07fc94836bcc8a3e72f
matt1432 <matt@nelim.org>
2024-12-26 01:16:39 -0500

    docs(gtk3): fix actionGroup

--- examples/js/simple-bar/widget/Bar.tsx
+++ examples/js/simple-bar/widget/Bar.tsx
@@ -16,7 +16,7 @@ function SysTray() {
             <menubutton
                 tooltipMarkup={bind(item, "tooltipMarkup")}
                 usePopover={false}
-                actionGroup={bind(item, "action-group").as(ag => ["dbusmenu", ag])}
+                insertActionGroup={bind(item, "action-group").as(ag => ["dbusmenu", ag])}
                 menuModel={bind(item, "menu-model")}>
                 <icon gicon={bind(item, "gicon")} />
             </menubutton>
